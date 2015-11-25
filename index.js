ea894edabaa2f3bc078c214c75dd8230382df3d4
Diego Haz <hazdiego@gmail.com>
2015-11-24 22:01:36 -0200

    Many changes

--- index.js
+++ index.js
@@ -4,4 +4,9 @@ var app = express();
 app.use('/', express.static(__dirname + '/public'));
 app.use('/bower_components', express.static(__dirname + '/bower_components'));
 
+app.get(/^((?!bower_components).)*$/, function(req, res) {
+  res.set('Content-Type', 'text/html');
+  res.sendfile(__dirname + '/public/index.html');
+});
+
 app.listen(process.env.PORT || 8080);
